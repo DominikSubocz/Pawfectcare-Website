267c08eb9d08a3f57eb0171f87e6a153d10d6a81
Dominik Subocz <dominiksubocz@gmail.com>
2024-03-15 10:56:12 +0000

    Small fixes & Testing
    Testing documentation and small fixes

--- checkout.php
+++ checkout.php
@@ -14,14 +14,15 @@ $basket = Basket::getBasketArray();
 
 $output = "";
 
-if ($_SERVER["REQUEST_METHOD"] === "POST"){
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["submitOrder"])) {
     require("classes/order.php");
 
     $output = Order::validate();
 
-    if(!$output){
+    if (!$output) {
         Order::create($basket);
         header("Location: " . Utils::$projectFilePath . "/user.php");
+        exit; // Add exit after header redirection
     }
 }
 
@@ -31,63 +32,50 @@ components::pageHeader("Checkout", ["style"], ["mobile-nav"]);
 
 <main class="content-wrapper home-content">
 
+    <h2>Checkout</h2>
 
-<h2>Checokout</h2>
+    <form
+        method="POST"
+        action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"
+        enctype="multipart/form-data"
+        class="form"
+    >
+        <h3>Shipping Address</h3>
+        <label>Address Line</label>
+        <input type="text" name="address_line" value="<?php echo htmlspecialchars($_POST["address_line"] ?? ''); ?>"> 
+        <input type="text" name="town" value="<?php echo htmlspecialchars($_POST["town"] ?? ''); ?>"> 
+        <input type="text" name="county" value="<?php echo htmlspecialchars($_POST["county"] ?? ''); ?>"> 
+        <input type="text" name="postcode" value="<?php echo htmlspecialchars($_POST["postcode"] ?? ''); ?>"> 
 
-<form
-    method="POST"
-    action="<?php echo $_SERVER["PHP_SELF"]; ?>"
-    enctype="multipart/form-data"
-    class="form"
->
-    <h3>Shipping Address</h3>
-    <label>Address Line</label>
-    <input type="text" name="address_line" value="<?php if ($output){
-        echo $_POST["address_line"];
-    }  ?>">
-    
-    <input type="text" name="town" value="<?php if ($output){
-        echo $_POST["town"];
-    }  ?>">
+        <h3>Payment Details</h3>
 
-    <input type="text" name="county" value="<?php if ($output){
-        echo $_POST["county"];
-    }  ?>">
+        <label>Card Number</label>
+        <input type="text" name="card_number">
 
-    <input type="text" name="postcode" value="<?php if ($output){
-        echo $_POST["postcode"];
-    }  ?>">
+        <div class="form-row">
+            <div>
+                <label>Expiry</label>
 
-    <h3>Payment Details</h3>
+                <div class="inline-inputs">
+                    <input type="text" name="month" placeholder="month">
+                    <input type="text" name="year" placeholder="year">
+                </div>
+            </div>
 
-    <label>Card Number</label>
-    <input type="text" name="card_number">
-
-    <div class="form-row">
-        <div>
-            <label>Expiry</label>
-
-            <div class="inline-inputs">
-                <input type="text" name="month" placeholder="month">
-                <input type="text" name="year" placeholder="year">
+            <div>
+                <label>Security Number</label>
+                <input type="text" name="security_number">
             </div>
         </div>
 
-        <div>
-            <labeL>Security Number</label>
-            <input type="text" name="security_number">
-        </div>
-    </div>
-        <input class="button" type="submit" value="Complete order">
-        <?php if($oputput) { echo $output;} ?>
-</form>
+        <input class="button" type="submit" name="submitOrder" value="Complete order"> <!-- Fix type attribute -->
+        <?php if($output) { echo htmlspecialchars($output); } ?> <!-- Sanitize output -->
+    </form>
 
 </main>
+
 <?php
 
 Components::pageFooter();
 
 ?>
-
-
-
